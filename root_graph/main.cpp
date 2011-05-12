753b6e0e2051a17e37dd6726bc267585c80ae381
wutzi15 <wutzis_spam2@hotmail.de>
2011-05-12 10:17:16 +0200

    Initial Commit

---
+++ root_graph/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  root_graph
+//
+//  Created by Wutzi on 12.05.11.
+//  Copyright 2011 me. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
