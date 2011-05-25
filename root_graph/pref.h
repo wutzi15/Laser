8cc13ebdfe7b14fe58dfd073256e4783f59eb745
wutzi15 <wutzis_spam2@hotmail.de>
2011-05-25 17:15:14 +0200

    added files

---
+++ root_graph/pref.h
@@ -0,0 +1,34 @@
+//
+//  pref.h
+//  root_graph
+//
+//  Created by Wutzi on 13.05.11.
+//  Copyright 2011 me. All rights reserved.
+//
+#include "TApplication.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include <fstream>
+#include "TImage.H"
+#include <string>
+#include "TGraph2D.h"
+#include "TApplication.h"
+#include "TCanvas.h"
+#include "TROOT.h"
+#include "boost/filesystem.hpp"
+#include "boost/lexical_cast.hpp"
+#include <vector>
+#include <exception>
+#include "TView3D.h"
+#include "TBenchmark.h"
+#include "TSlider.h"
+#include <assert.h>
+#include <iostream>
+#include "TF1.h"
+#include "TSpectrum.h"
+#include "TVirtualFitter.h"
+#include "TMath.h"
+#include "TGraph.h"
+#include <iostream>
+#include <sstream>
+#include "TStyle.h"
